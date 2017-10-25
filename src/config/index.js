294ac91b380f0198d05e076bf7db1de8a4b16f71
Maique Rosa <maique.rosa@gmail.com>
2017-10-25 14:58:14 -0200

    pegando porta dinamicamente

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,7 @@
+const port = process.env.PORT || '3005';
 module.exports = {
-    port:'3005',
-    jsonLimit:'100kb',
-    mongooseUrl:process.env.MONGOLAB_URI ? process.env.MONGOLAB_URI  : 'mongodb://localhost:27017/marciadb',
+    port: port,
+    jsonLimit: '100kb',
+    mongooseUrl: process.env.MONGOLAB_URI ? process.env.MONGOLAB_URI : 'mongodb://localhost:27017/marciadb',
     secretKey: process.env.SECRET_KEY ? process.env.SECRET_KEY : 'S3c4E7-K3y-L0c4L'
 }

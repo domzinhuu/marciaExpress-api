6c58d1be2c312ecc3df4ed069a87ee948b9f1834
Maique Rosa <maique.rosa@gmail.com>
2017-10-26 01:49:53 -0200

    alterando configs

--- src/config/index.js
+++ src/config/index.js
@@ -3,5 +3,5 @@ module.exports = {
     port: port,
     jsonLimit: '100kb',
     mongooseUrl:'mongodb://localhost:27017/marciadb',
-    secretKey: 'S3c4E7-K3y-L0c4L-Al7e4aR'
+    secretKey: 'S3c4E7-K3y-L0c4L-Al7e4aRddd'
 }
